e089cb6712c0a08f9754feaca25524d445861b3b
Carl-Fredrik Herö <carlfredrik.hero@gmail.com>
2017-04-21 20:54:31 +0200

    AddRunner added

--- src/Runner.elm
+++ src/Runner.elm
@@ -3,6 +3,9 @@ module Runner exposing (..)
 import Html exposing (Html, form, input, div, h1, text, p, button, nav, a)
 import Html.Attributes exposing (type_, placeholder, value, class, href, classList)
 import Html.Events exposing (onInput, onSubmit)
+import Http exposing (..)
+import Json.Encode as JE
+import Json.Decode as JD exposing (field)
 
 
 -- model
@@ -22,19 +25,24 @@ type alias Model =
     }
 
 
+initModel : Model
+initModel =
+    { id = ""
+    , name = ""
+    , nameError = Nothing
+    , location = ""
+    , locationError = Nothing
+    , age = ""
+    , ageError = Nothing
+    , bib = ""
+    , bibError = Nothing
+    , error = Nothing
+    }
+
+
 init : ( Model, Cmd Msg )
 init =
-    ( { id = ""
-      , name = ""
-      , nameError = Nothing
-      , location = ""
-      , locationError = Nothing
-      , age = ""
-      , ageError = Nothing
-      , bib = ""
-      , bibError = Nothing
-      , error = Nothing
-      }
+    ( initModel
     , Cmd.none
     )
 
@@ -45,10 +53,11 @@ type Msg
     | AgeInput String
     | BibInput String
     | Save
+    | SaveResponse (Result Http.Error String)
 
 
-update : Msg -> Model -> ( Model, Cmd Msg )
-update msg model =
+update : String -> Msg -> Model -> ( Model, Cmd Msg )
+update token msg model =
     case msg of
         NameInput name ->
             ( { model
@@ -83,7 +92,170 @@ update msg model =
             )
 
         Save ->
-            ( model, Cmd.none )
+            let
+                updatedModel =
+                    validate model
+            in
+                if isValid updatedModel then
+                    save token updatedModel
+                else
+                    ( updatedModel, Cmd.none )
+
+        SaveResponse (Ok id) ->
+            ( initModel, Cmd.none )
+
+        SaveResponse (Err err) ->
+            let
+                errMsg =
+                    case err of
+                        Http.BadStatus resp ->
+                            resp.body
+
+                        _ ->
+                            "Error Saving!"
+            in
+                ( { model | error = Just errMsg }, Cmd.none )
+
+
+url : String
+url =
+    "http://localhost:5000/runner"
+
+
+save : String -> Model -> ( Model, Cmd Msg )
+save token model =
+    let
+        headers =
+            [ Http.header "Authorization" ("Bearer " ++ token) ]
+
+        body =
+            Http.jsonBody <| runnerEncoder model
+
+        decoder =
+            field "_id" JD.string
+
+        request =
+            post url headers body decoder
+
+        cmd =
+            Http.send SaveResponse request
+    in
+        ( model, cmd )
+
+
+runnerEncoder : Model -> JE.Value
+runnerEncoder { name, location, age, bib } =
+    let
+        ageInt =
+            age
+                |> String.toInt
+                |> Result.withDefault 0
+
+        bibInt =
+            bib
+                |> String.toInt
+                |> Result.withDefault 0
+    in
+        JE.object
+            [ ( "name", JE.string name )
+            , ( "location", JE.string location )
+            , ( "age", JE.int ageInt )
+            , ( "bib", JE.int bibInt )
+            ]
+
+
+post : String -> List Http.Header -> Http.Body -> JD.Decoder a -> Http.Request a
+post url headers body decoder =
+    Http.request
+        { method = "POST"
+        , url = url
+        , headers = headers
+        , body = body
+        , expect = Http.expectJson decoder
+        , timeout = Nothing
+        , withCredentials = False
+        }
+
+
+isValid : Model -> Bool
+isValid model =
+    model.nameError
+        == Nothing
+        && model.locationError
+        == Nothing
+        && model.ageError
+        == Nothing
+        && model.bibError
+        == Nothing
+
+
+validate : Model -> Model
+validate model =
+    model
+        |> validateName
+        |> validateLocation
+        |> validateAge
+        |> validateBib
+
+
+validateName : Model -> Model
+validateName model =
+    if String.isEmpty model.name then
+        { model
+            | nameError = Just "Name is required"
+        }
+    else
+        { model
+            | nameError = Nothing
+        }
+
+
+validateLocation : Model -> Model
+validateLocation model =
+    if String.isEmpty model.location then
+        { model
+            | locationError = Just "Location is required"
+        }
+    else
+        { model
+            | locationError = Nothing
+        }
+
+
+validateAge : Model -> Model
+validateAge model =
+    let
+        ageInt =
+            model.age
+                |> String.toInt
+                |> Result.withDefault 0
+    in
+        if ageInt <= 0 then
+            { model
+                | ageError = Just "Age must be a positive number"
+            }
+        else
+            { model
+                | ageError = Nothing
+            }
+
+
+validateBib : Model -> Model
+validateBib model =
+    let
+        bibInt =
+            model.bib
+                |> String.toInt
+                |> Result.withDefault 0
+    in
+        if bibInt <= 0 then
+            { model
+                | bibError = Just "Bib must be a positive number"
+            }
+        else
+            { model
+                | bibError = Nothing
+            }
 
 
 view : Model -> Html Msg
@@ -96,7 +268,6 @@ view model =
                 [ textInputField
                     "Name"
                     [ value model.name
-                    , class "greetings"
                     , onInput NameInput
                     , placeholder "Name"
                     ]
@@ -128,10 +299,9 @@ view model =
                     , placeholder "Bib #"
                     , Html.Attributes.min "0"
                     ]
-                , buttonInputField
-                    "Save"
+                , submitInputField
                     [ class "button is-primary"
-                    , type_ "button"
+                    , type_ "submit"
                     , value "Save"
                     ]
                 ]
@@ -164,6 +334,7 @@ textInputField label attr error =
                     ((classList cls) :: (type_ "text") :: attr)
                     []
                 ]
+            , p [ class "help is-danger" ] [ text error ]
             ]
 
 
@@ -179,13 +350,13 @@ numberInputField label attr =
         ]
 
 
-buttonInputField : String -> List (Html.Attribute Msg) -> Html Msg
-buttonInputField label attr =
+submitInputField : List (Html.Attribute Msg) -> Html Msg
+submitInputField attr =
     div [ class "field" ]
         [ p [ class "control" ]
-            [ button
+            [ input
                 attr
-                [ text label ]
+                []
             ]
         ]
 
